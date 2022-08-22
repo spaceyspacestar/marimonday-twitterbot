528f1228a4da6fcafbcf146e642149b01cf9f896
Space Fujisaki <41171313+spaceyspacestar@users.noreply.github.com>
2022-08-22 10:24:46 -0700

    This was updated in school lol

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+from datetime import date #Important for pythonanywhere tasks
 import tweepy #API Stuff
 import json
-
-#Scheduling
-import schedule
-import time
 
 with open("auth.json", "r") as file:
     data = json.load(file)
@@ -19,10 +16,11 @@ ownerid = data['owner_id']
 #Main course
 def tweet():
     #Login
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     #Upload video to the site
-    media = api.media_upload("Mari.mp4")
+    media = api.media_upload(filename="./Mari.mp4", chunked=True, media_category='tweet_video')
     try:
         api.update_status(status="", media_ids=[media.media_id])
         print("Successfully posted video")
@@ -33,17 +31,20 @@ def tweet():
 #Make sure it's actually running since it's running in docker
 def checkifrunning():
     #Login
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    #lol
-    api.send_direct_message(recipient_id=ownerid, text="This message is to ensure the program is running correctly!")
+    #Weekly message check up
+    api.send_direct_message(recipient_id=ownerid, text="This weekly message is to ensure the pythonanywhere server is still running!")
 
 #Actually post the video to the site
 if __name__ == '__main__':
-    #First check if the program is still alive
-    #then we can schedule the time
-    schedule.every.monday.at("04:00").do(tweet)
-    schedule.every.wednesday.at("16:00").do(checkifrunning) #Check if the heroku server is alive
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    #Check if today is monday
+    if(date.today().weekday() == 0):
+        tweet()
+    elif(date.today().weekday() == 2):
+        #Feel free to comment these three lines out if you're confident it is running still
+        print("Update day, check up on bot owner")
+        checkifrunning()
+    else:
+        print("Today is not a Monday or a Wednesday, Exit bot")
